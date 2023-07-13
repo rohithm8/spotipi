6f101253eb23f8c589ee8b693d716c8f611f26ef
Rohith Manikonda <rohith.manikonda@keble.ox.ac.uk>
2023-07-14 00:25:11 +0100

    Support for Night weather icons

--- python/noSongImage.py
+++ python/noSongImage.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont, ImageColor
 import python_weather
 import os
-from datetime import date
+from datetime import date, datetime
 import time
 import asyncio
 
@@ -18,16 +18,20 @@ def noSongImage(size=(64, 64), weather_location="London"):
     font = ImageFont.truetype(os.path.join(dir, '../fonts/5x7.ttf'), 8)
     timetext = time.strftime("%H:%M", time.localtime())
     datetext = date.today().strftime("%b %d")
-    draw.text((0, 0), timetext, fill=ImageColor.getrgb("white"), font=font)
-    draw.text((0, 8), datetext, fill=ImageColor.getrgb("white"), font=font)
+    draw.text((1, 0), timetext, fill=ImageColor.getrgb("white"), font=font)
+    draw.text((1, 8), datetext, fill=ImageColor.getrgb("white"), font=font)
     weather = asyncio.run(getWeather(weather_location))
     currentWeather, forecastWeather = weather.current, weather.forecasts
     tempText = str(currentWeather.temperature) + "C"
-    # print(currentWeather.kind.emoji)
-    draw.text((63, 0), tempText, fill=ImageColor.getrgb("white"), font=font, anchor="ra")
-    weatherIcon = Image.open(os.path.join(dir, f'../images/{str(currentWeather.kind)}.png')).convert("RGBA")
+    sunSet = next(forecastWeather).astronomy.sun_set
+    sunRise = next(forecastWeather).astronomy.sun_rise
+    if sunSet < datetime.now().time() < sunRise:
+        thumbFilename = os.path.join(dir, f'../images/{str(currentWeather.kind)}.png')
+    else:
+        thumbFilename = os.path.join(dir, f'../images/{str(currentWeather.kind)} Night.png')
+    draw.text((64, 0), tempText, fill=ImageColor.getrgb("white"), font=font, anchor="ra")
+    weatherIcon = Image.open(thumbFilename).convert("RGBA")
     image.alpha_composite(weatherIcon, (64-20, 8))
-    # draw.text((63, 8), str(currentWeather.kind), fill=ImageColor.getrgb("white"), font=font, anchor="ra")
     return image
 
 # noSongImage().convert("RGB").save("noSongImage.png") # for testing
