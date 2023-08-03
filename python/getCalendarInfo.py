a88049001166e9ef3003bedf1ce779291e38c516
Rohith Manikonda <rohith.manikonda@keble.ox.ac.uk>
2023-08-03 21:09:04 +0100

    Add script to fetch calendar info

---
+++ python/getCalendarInfo.py
@@ -0,0 +1,65 @@
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+dir = os.path.join(os.path.dirname(__file__), '../config')
+
+
+def getCalendarInfo():
+    """
+    Returns the next calendar event
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(os.path.join(dir, 'token.json')):
+        creds = Credentials.from_authorized_user_file(os.path.join(dir, 'token.json'), SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                os.path.join(dir, 'credentials.json'), SCOPES)
+            creds = flow.run_console()
+        # Save the credentials for the next run
+        with open(os.path.join(dir, 'token.json'), 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow()  # 'Z' indicates UTC time
+        later = now + datetime.timedelta(days=1)
+        events_result = service.events().list(calendarId='primary', timeMin=now.isoformat() + 'Z',
+                                              timeMax=later.isoformat() + 'Z',
+                                              maxResults=1, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            # print('No upcoming events found.')
+            return None
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            return (start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+        return None
+
+
+
+if __name__ == '__main__':
+    print(getCalendarInfo())
