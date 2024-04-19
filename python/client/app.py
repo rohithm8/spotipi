7070882da0d17af16e800410e92c9b2e717e4f77
Rohith Manikonda <rohith.manikonda@keble.ox.ac.uk>
2024-04-19 12:35:51 +0100

    Add new features to frontend

--- python/client/app.py
+++ python/client/app.py
@@ -29,7 +29,10 @@ def saved_config():
     height = int(config['DEFAULT']['columns'])
     power = config['DEFAULT']['power']
     refresh_rate = config['DEFAULT']['refresh_rate']
-    return render_template('index.html', brightness = brightness, width = width, height = height, power = power, refresh_rate = refresh_rate)
+    weather_location = config['DEFAULT']['weather_location']
+    schedule_start = config['DEFAULT']['schedule_start']
+    schedule_end = config['DEFAULT']['schedule_end']
+    return render_template('index.html', brightness = brightness, width = width, height = height, power = power, refresh_rate = refresh_rate, weather_location = weather_location, schedule_start=schedule_start, schedule_end=schedule_end)
 
 # handling power status
 @app.route("/power", methods=["GET", "POST"])
@@ -38,12 +41,15 @@ def handle_power():
     brightness = int(config['DEFAULT']['brightness'])
     width = int(config['DEFAULT']['rows'])
     height = int(config['DEFAULT']['columns'])
+    weather_location = config['DEFAULT']['weather_location']
+    schedule_start = config['DEFAULT']['schedule_start']
+    schedule_end = config['DEFAULT']['schedule_end']
     config.set('DEFAULT', 'power', request.form['power'])
     if power == 'on':
       job = manager.StartUnit('spotipi.service', 'replace')
     else:
       job = manager.StopUnit('spotipi.service', 'replace')
-    return render_template('index.html', brightness = brightness, width = width, height = height, power = power)
+    return render_template('index.html', brightness = brightness, width = width, height = height, weather_location = weather_location, power = power, schedule_start=schedule_start, schedule_end=schedule_end)
 
 # handling form data
 @app.route('/brightness', methods=['POST'])
@@ -52,10 +58,13 @@ def handle_brightness():
     width = int(config['DEFAULT']['rows'])
     height = int(config['DEFAULT']['columns'])
     power = config['DEFAULT']['power']
+    weather_location = config['DEFAULT']['weather_location']
+    schedule_start = config['DEFAULT']['schedule_start']
+    schedule_end = config['DEFAULT']['schedule_end']
     with open(filename, 'w') as configfile:
         config.write(configfile)
     job = manager.RestartUnit('spotipi.service', 'fail')
-    return render_template('index.html', brightness = request.form['brightness'], width = width, height = height, power = power)
+    return render_template('index.html', brightness = request.form['brightness'], width = width, height = height, weather_location = weather_location, power = power, schedule_start=schedule_start, schedule_end=schedule_end)
 
 # handling form data
 @app.route('/size', methods=['POST'])
@@ -64,10 +73,13 @@ def handle_size():
     config.set('DEFAULT', 'columns', request.form['height'])
     brightness = int(config['DEFAULT']['brightness'])
     power = config['DEFAULT']['power']
+    weather_location = config['DEFAULT']['weather_location']
+    schedule_start = config['DEFAULT']['schedule_start']
+    schedule_end = config['DEFAULT']['schedule_end']
     with open(filename, 'w') as configfile:
         config.write(configfile)
     job = manager.RestartUnit('spotipi.service', 'fail')
-    return render_template('index.html', brightness = brightness, width = int(request.form['width']), height = int(request.form['height']), power = power)
+    return render_template('index.html', brightness = brightness, width = int(request.form['width']), height = int(request.form['height']), weather_location = weather_location, power = power, schedule_start=schedule_start, schedule_end=schedule_end)
 
 # handling form data
 @app.route('/refresh-rate', methods=['POST'])
@@ -77,10 +89,48 @@ def handle_refresh_rate():
     power = config['DEFAULT']['power']
     width = int(config['DEFAULT']['rows'])
     height = int(config['DEFAULT']['columns'])
+    weather_location = config['DEFAULT']['weather_location']
+    schedule_start = config['DEFAULT']['schedule_start']
+    schedule_end = config['DEFAULT']['schedule_end']
     with open(filename, 'w') as configfile:
         config.write(configfile)
     job = manager.RestartUnit('spotipi.service', 'fail')
-    return render_template('index.html', brightness = brightness, width = width, height = height, refresh_rate = int(request.form['refresh_rate']), power = power)
+    return render_template('index.html', brightness = brightness, width = width, height = height, refresh_rate = int(request.form['refresh_rate']), weather_location = weather_location, power = power, schedule_start=schedule_start, schedule_end=schedule_end)
+
+# handling form data
+@app.route('/location', methods=['POST'])
+def handle_location():
+    config.set('DEFAULT', 'weather_location', request.form['weather_location'].replace(" ", "+"))
+    brightness = int(config['DEFAULT']['brightness'])
+    width = int(config['DEFAULT']['rows'])
+    height = int(config['DEFAULT']['columns'])
+    power = config['DEFAULT']['power']
+    refresh_rate = config['DEFAULT']['refresh_rate']
+    schedule_start = config['DEFAULT']['schedule_start']
+    schedule_end = config['DEFAULT']['schedule_end']
+    with open(filename, 'w') as configfile:
+        config.write(configfile)
+    job = manager.RestartUnit('spotipi.service', 'fail')
+    return render_template('index.html', brightness = brightness, width = width, height = height, refresh_rate = refresh_rate, weather_location = request.form['weather_location'], power = power, schedule_start=schedule_start, schedule_end=schedule_end)
+
+
+
+
+@app.route('/schedule', methods=['POST'])
+def handle_schedule():
+    config.set('DEFAULT', 'schedule_start', request.form['schedule_start'])
+    config.set('DEFAULT', 'schedule_end', request.form['schedule_end'])
+    brightness = int(config['DEFAULT']['brightness'])
+    width = int(config['DEFAULT']['rows'])
+    height = int(config['DEFAULT']['columns'])
+    power = config['DEFAULT']['power']
+    refresh_rate = config['DEFAULT']['refresh_rate']
+    weather_location = config['DEFAULT']['weather_location']
+    with open(filename, 'w') as configfile:
+        config.write(configfile)
+    job = manager.RestartUnit('spotipi.service', 'fail')
+    return render_template('index.html', brightness=brightness, width=width, height=height, refresh_rate=refresh_rate, weather_location=weather_location, power=power, schedule_start=request.form['schedule_start'], schedule_end=request.form['schedule_end'])
+
 
 app.run(host='0.0.0.0', port=80) 
 
